26bd3742e80b728b10db1ee6d39b929abdd8d5cc
Stewart Tran <stewart.tran@avepoint.com>
2023-11-30 21:45:02 +0700

    add user controller 3

--- Application.Contract/Channels/IChannelService.cs
+++ Application.Contract/Channels/IChannelService.cs
@@ -11,15 +11,16 @@ namespace PBL6.Application.Contract.Channels
         Task<Guid> UpdateAsync(Guid channelId, UpdateChannelDto updateChannelDto);
         Task<Guid> AddAsync(CreateChannelDto createUpdateChannelDto);
         Task<Guid> DeleteAsync(Guid channelId);
-        Task<IEnumerable<ChannelRoleDto>> GetRolesAsync(Guid channelId);
         Task<Guid> AddMemberToChannelAsync(Guid channelId, List<Guid> userIds);
         Task<Guid> RemoveMemberFromChannelAsync(Guid channelId, List<Guid> userIds);
+        Task<IEnumerable<ChannelRoleDto>> GetRolesAsync(Guid channelId);
         Task<IEnumerable<PermissionDto>> GetPermissionsAsync(Guid channelId, Guid roleId);
         Task<IEnumerable<PermissionDto>> GetPermissions();
         Task UpdateRoleAsync(Guid channelId, Guid roleId, CreateUpdateChannelRoleDto input);
         Task<Guid> AddRoleAsync(Guid channelId, CreateUpdateChannelRoleDto input);
         Task DeleteRoleAsync(Guid channelId, Guid roleId);
         Task SetRoleToUserAsync(Guid channelId, Guid userId, Guid role);
+        Task<ChannelRoleDto> GetRoleAsync(Guid channelId, Guid roleId);
         Task<IEnumerable<PermissionDto>> GetPermissionOfUser(Guid channelId, Guid userId);
     }
 }
