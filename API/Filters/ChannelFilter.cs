3150308f1bdc1b5ff948edea77f222fa1caf8850
Stewart Tran <stewart.tran@avepoint.com>
2023-12-29 11:04:18 +0700

    add channel filter, channel will not available if ws is suspended

--- API/Filters/ChannelFilter.cs
+++ API/Filters/ChannelFilter.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc.Filters;
 using PBL6.Application.Contract.Channels;
+using PBL6.Application.Contract.Workspaces;
+using PBL6.Common.Enum;
 using PBL6.Common.Exceptions;
 using PBL6.Common.Functions;
 
@@ -15,6 +17,10 @@ namespace PBL6.API.Filters
             _policyName = policyName;
         }
 
+        public ChannelFilter()
+        {
+        }
+
         public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
         {
             var channelId = GetValue(context, "channel-id");
@@ -25,17 +31,29 @@ namespace PBL6.API.Filters
 
             var channelService =
                 context.HttpContext.RequestServices.GetService<IChannelService>();
+            var workspaceService =
+                context.HttpContext.RequestServices.GetService<IWorkspaceService>();
+            var channel = await channelService.GetByIdAsync(Guid.Parse(channelId));
+            var workspace = await workspaceService.GetByIdAsync(channel.WorkspaceId);
 
-            var userId = context.HttpContext.User.Claims
-                .FirstOrDefault(x => x.Type == CustomClaimTypes.UserId)
-                ?.Value;
-            var permissions = await channelService.GetPermissionOfUser(
-                Guid.Parse(channelId),
-                Guid.Parse(userId)
-            );
-            if (!permissions.Any(x => x.Code == _policyName))
+            if (workspace.Status == (short)WORKSPACE_STATUS.SUSPENDED)
             {
-                throw new ForbidException();
+                throw new SuspendedWorkspaceException();
+            }
+            
+            if (_policyName != string.Empty)
+            {
+                var userId = context.HttpContext.User.Claims
+                    .FirstOrDefault(x => x.Type == CustomClaimTypes.UserId)
+                    ?.Value;
+                var permissions = await channelService.GetPermissionOfUser(
+                    Guid.Parse(channelId),
+                    Guid.Parse(userId)
+                );
+                if (!permissions.Any(x => x.Code == _policyName))
+                {
+                    throw new ForbidException();
+                }
             }
         }
 
